940de7070ab12958346bec2fcd909367c63ce71b
gh.guo <747237971@qq.com>
2020-05-14 08:59:27 +0800

    Update sync_upstream_repo.sh

--- sync_upstream_repo.sh
+++ sync_upstream_repo.sh
@@ -3,7 +3,7 @@ echo '------------START-------------'
 set -x
 git pull
 git submodule update --remote
-git add . && git commit -m '[*]Auto sync upstream repo'
+git add . && git commit -m '[SHELL]Auto sync upstream repo'
 git push
 set +x
 echo '-----------END--------------'
