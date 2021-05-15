dfa8fddd172ea3b96daeba6b23b6eb0ca9f88674
hootigger <hootigger8@gmai.com>
2021-05-15 14:11:20 +0800

    [SHELL]Auto sync upstream repo

--- sync_upstream_repo.sh
+++ sync_upstream_repo.sh
@@ -5,5 +5,12 @@ git pull
 git submodule update --remote
 git add . && git commit -m '[SHELL]Auto sync upstream repo'
 git push
+if [ ! -d ../lede ]; then
+	git clone https://github.com/coolsnowwolf/lede.git ../lede
+fi
+cd ../lede
+git pull
+./scripts/feed update && ./scripts/feed install
+make menuconfig
 set +x
 echo '-----------END--------------'
